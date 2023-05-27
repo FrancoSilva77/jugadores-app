d0a447121c2fc5c598452d034a29014b8053546d
Franco Silva <franco_tigres18@hotmail.com>
2023-05-27 11:31:44 -0600

    Variables de entorno add

--- config/db.js
+++ config/db.js
@@ -1,8 +1,11 @@
 import  Sequelize  from "sequelize";
+import dotenv from 'dotenv';
 
-const db = new Sequelize('jugadores', 'root', 'root', {
-  host: '127.0.0.1',
-  port: '3306',
+dotenv.config({path: '.env'})
+
+const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   dialect: 'mysql',
   define: {
     timestamps: false
